545951decaa9b721592e7a9c0e82abb9dc6ea292
Camden Hovell <camden.hovell@bsu.edu>
2024-10-09 22:44:16 -0400

    Fix flashlight shooting z direction

--- project/hand/hand.gd
+++ project/hand/hand.gd
@@ -7,9 +7,9 @@ var min_throw_angle : int = -90
 var max_throw_angle : int = 90
 var _y_direction_power : int = 5
 var _x_offset : int = -10
+var _z_direction : int
 
 @onready var _flashlight_object : Flashlight
-
 
 func _ready() -> void:
 	call_deferred("_spawn_flashlight")
@@ -20,8 +20,10 @@ func _physics_process(_delta: float) -> void:
 		_throw_flashlight()
 	elif Input.is_action_pressed("move_left"):
 		_flashlight_object.rotation_degrees.y += 1
+		_z_direction -= 1
 	elif Input.is_action_pressed("move_right"):
 		_flashlight_object.rotation_degrees.y -= 1
+		_z_direction += 1
 	elif Input.is_action_pressed("move_up"):
 		_flashlight_object.rotation_degrees.x += 1
 	elif Input.is_action_pressed("move_down"):
@@ -31,15 +33,19 @@ func _physics_process(_delta: float) -> void:
 		_flashlight_object.queue_free()
 		_spawn_flashlight()
 
+
 func _spawn_flashlight() -> void:
 	_flashlight_object = load("res://flashlight/flashlight.tscn").instantiate()
 	get_parent().add_child(_flashlight_object)
 	_flashlight_object.global_position = global_position
 	_flashlight_object.global_rotation_degrees.y = -90
+	_x_offset = 0
+	throw_y_direction = 0
+	_z_direction = 0
 
 
 func _throw_flashlight() -> void:
-	var _impulse := Vector3(throw_strength,throw_y_direction * _y_direction_power, 0)
+	var _impulse := Vector3(throw_strength,throw_y_direction * _y_direction_power, _z_direction)
 	var _position_normalized := Vector3(_x_offset,throw_y_direction, 0).normalized()
 	_flashlight_object.apply_impulse(_impulse, _position_normalized)
 	_flashlight_object.gravity_scale = 1
